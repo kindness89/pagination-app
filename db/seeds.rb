6a63f20c298ce2dcdc50e697b91aff11957d2e86
kindness89 <yuusan29blog@gmail.com>
2021-08-07 23:33:34 +0900

    Rails 課題4終了

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Faker::Config.locale = :ja
+
+50.times do
+  Post.create!(body: Faker::JapaneseMedia::OnePiece.akuma_no_mi)
+end
+
+puts "悪魔の実を50個集めました"
